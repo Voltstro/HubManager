85f6d3a0bbecc5acee20c01b5fe91bc7ff24afef
Voltstro <me@voltstro.dev>
2019-04-30 16:37:17 +1000

    Can now reload

--- src/org/australiacraft/hubmanager/Main.java
+++ src/org/australiacraft/hubmanager/Main.java
@@ -19,6 +19,7 @@ public class Main extends JavaPlugin {
 		pm.addPermission(sethubPerm);
 		
 		getCommand("sethub").setExecutor(new SetHubCommand(this));
+		getCommand("hubmanager").setExecutor(new HubManagerCommands(this));
 	}
 	
 	public void loadConfig() {
@@ -31,12 +32,11 @@ public class Main extends JavaPlugin {
 			
 			String cmd = baseCommands.get(i).toString();
 			
-			getConfig().addDefault(cmd + ".message", "You have been teleported to %name%!%line%You can change this message in the config.");
+			getConfig().addDefault(cmd + ".message", "\u00A76You have been teleported to \u00A7c%name%\u00A76!%line%You can change this message in the config.");
 			getConfig().addDefault(cmd + ".world", "");
 			getConfig().addDefault(cmd + ".x", 0);
 			getConfig().addDefault(cmd + ".y", 0);
 			getConfig().addDefault(cmd + ".z", 0);
-			
 			
 			((CraftServer) this.getServer()).getCommandMap().register(cmd, new HubCommand(cmd, this));
 			
